efc6588ff50604890b56bece75ea944c354bbd2e
chubbystrings <martinsokwor@hotmail.com>
2020-08-08 12:42:52 +0100

    fix for service account

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,6 +1,18 @@
+/* eslint-disable quote-props */
 const admin = require('firebase-admin');
 
-const serviceAccount = require('./serviceaccountkey.json');
+const serviceAccount = {
+  'type': process.env.SERVICE_TYPE,
+  'project_id': process.env.SERVICE_PROJECT_ID,
+  'private_key_id': process.env.SERVICE_PRIVATE_KEY_ID,
+  'private_key': process.env.SERVICE_PRIVATE_KEY,
+  'client_email': process.env.SERVICE_CLIENT_EMAIL,
+  'client_id': process.env.SERVICE_CLIENT_ID,
+  'auth_uri': process.env.SERVICE_AUTH_URI,
+  'token_uri': process.env.SERVICE_TOKEN_URI,
+  'auth_provider_x509_cert_url': process.env.SERVICE_AUTH_PROVIDER_CERT,
+  'client_x509_cert_url': process.env.SERVICE_CLIENT_CERT,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
