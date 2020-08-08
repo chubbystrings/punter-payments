58b12fc7a028e08a5d1d15ca4d297d268f30e5f9
chubbystrings <martinsokwor@hotmail.com>
2020-08-08 11:45:54 +0100

    heroku error fix 2

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,6 +1,6 @@
 const admin = require('firebase-admin');
 
-const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccount = require('./serviceaccountkey.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
